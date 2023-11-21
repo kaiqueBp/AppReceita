4a26bd7c6c1b680b662f2290dfbb51280bd105d5
KaiqueBp <kpbarros2@outlook.com>
2023-11-21 19:22:45 -0400

    menu lateral adicionado

--- app/src/main/java/com/kaique/p2/MainActivity.java
+++ app/src/main/java/com/kaique/p2/MainActivity.java
@@ -1,14 +1,37 @@
 package com.kaique.p2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements CaracterList.OnListSelected {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=findViewById(R.id.container_root);
+        navigationView= findViewById(R.id.navView);
+
+        toggle= new ActionBarDrawerToggle(this,drawerLayout,R.string.fechar,R.string.abrir);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -16,6 +39,12 @@ public class MainActivity extends AppCompatActivity implements CaracterList.OnLi
                     .add(R.id.container_root, new CaracterList(), "characterList")
                     .commit();
         }
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
